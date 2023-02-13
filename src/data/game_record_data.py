7b17429c3ba139c0f8516710d04700522271c0bf
Agrillen <s213637@dtu.dk>
2023-02-13 14:51:32 +0100

    added code to record data with game
    Modified and restructured code of Nuria. Changed the visualization by changing the colors and adding an image of the movement.

---
+++ src/data/game_record_data.py
@@ -0,0 +1,280 @@
+import os
+import pickle
+import time
+from datetime import datetime
+
+import cv2
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from pandas import DataFrame
+from pylsl import StreamInlet, resolve_stream
+
+# https://docs.google.com/document/d/e/2PACX-1vR_4DXPTh1nuiOwWKwIZN3NkGP3kRwpP4Hu6fQmy3jRAOaydOuEI1jket6V4V6PG4yIG15H1N7oFfdV/pub
+from src.data.signal_processing import apply_mix, bandpass, logvar
+
+
+USER = 'anna'  # This is the username
+BOX_MOVE = "random"  # random or model
+
+font = cv2.FONT_HERSHEY_COMPLEX
+
+
+
+## ------------ FUNCTIONS ------------------------------
+# Create game
+def create_game_settings(WIDTH, HEIGHT, SQ_SIZE):
+    game_settings = {}
+    game_settings['square_1'] = {'x1': int(int(WIDTH) / 4 - int(SQ_SIZE / 2)),
+                'x2': int(int(WIDTH) / 4 + int(SQ_SIZE / 2)),
+                'y1': int(int(HEIGHT) / 10 * 9 - int(SQ_SIZE / 2)),
+                'y2': int(int(HEIGHT) / 10 * 9 + int(SQ_SIZE / 2))}
+
+    game_settings['square_2'] = {'x1': int(int(WIDTH) / 4 * 3 - int(SQ_SIZE / 2)),
+                'x2': int(int(WIDTH) / 4 * 3 + int(SQ_SIZE / 2)),
+                'y1': int(int(HEIGHT) / 10 * 9 - int(SQ_SIZE / 2)),
+                'y2': int(int(HEIGHT) / 10 * 9 + int(SQ_SIZE / 2))}
+
+    color_box_1 = [16/255, 128/255, 255/255]    # dark blue
+    color_box_2 = [255/255, 95/255, 1/255]    # dark orange
+    game_settings['box_1'] = np.ones((game_settings['square_1']['y2'] - game_settings['square_1']['y1'], game_settings['square_1']['x2'] - game_settings['square_1']['x1'], 3)) * np.array(color_box_1)
+    game_settings['box_2'] = np.ones((game_settings['square_2']['y2'] - game_settings['square_2']['y1'], game_settings['square_2']['x2'] - game_settings['square_2']['x1'], 3)) * np.array(color_box_2)
+
+    color_line_1 = [144/255, 196/255, 255/255]    # bright blue
+    color_line_2 = [255/255, 177/255, 131/255]    # bright orange
+
+    game_settings['vertical_line'] = np.ones((HEIGHT, 10, 3)) * np.array(color_line_1)
+    game_settings['vertical_line_2'] = np.ones((HEIGHT, 10, 3)) * np.array(color_line_2)
+    # game_settings['horizontal_line'] = np.ones((10, WIDTH, 3)) * np.random.uniform(size=(3,))
+
+    img = cv2.imread('C:/Users/annag/OneDrive - Danmarks Tekniske Universitet/Semester_04/Special_Course_BCI/03_code/BCI_stroke_rehab/data/external/arm.png')/255
+    # Downscale image
+    scale_percent = 40 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    game_settings['img_arm'] = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+    game_settings['WIDTH'] = WIDTH
+    game_settings['HEIGHT'] = HEIGHT
+    game_settings['SQ_SIZE'] = SQ_SIZE
+    
+    return game_settings
+
+
+def environment(game_settings):
+    env = np.zeros((game_settings['HEIGHT'], game_settings['WIDTH'], 3))
+    env[:, game_settings['WIDTH'] // 4 - 5:game_settings['WIDTH'] // 4 + 5, :] = game_settings['vertical_line']
+    env[:, game_settings['WIDTH'] // 4 * 3 - 5:game_settings['WIDTH'] // 4 * 3 + 5, :] = game_settings['vertical_line_2']
+    env[game_settings['square_1']['y1']:game_settings['square_1']['y2'], game_settings['square_1']['x1']:game_settings['square_1']['x2'], :] = game_settings['box_1']
+    env[game_settings['square_2']['y1']:game_settings['square_2']['y2'], game_settings['square_2']['x1']:game_settings['square_2']['x2'], :] = game_settings['box_2']
+    return env
+
+
+def update(pos, game_settings, actions):
+    env = environment(game_settings)
+
+    # Add arm image
+    shape = game_settings['img_arm'].shape
+    start_x = int(game_settings['WIDTH'] / 2 - 70)
+    start_y = int(game_settings['HEIGHT'] // 4 + 150)
+    env[start_y:start_y+shape[0], start_x:start_x+shape[1],:] = game_settings['img_arm']
+    
+    if actions[pos] == "right":
+        color = (255/255, 95/255, 1/255)
+    elif actions[pos] == "left":
+        color = (16/255, 128/255, 255/255)
+    else:
+        color = (255, 255, 255)
+    cv2.putText(env, "Task " + str(pos) + ":", (int(game_settings['WIDTH'] / 2 - 70), int(game_settings['HEIGHT'] // 4)), font, 1, (255, 255, 255),2)  # text,coordinate,font,size of text,color,thickness of font
+    cv2.putText(env, str(actions[pos]), (int(game_settings['WIDTH'] / 2 - 70), int(game_settings['HEIGHT'] // 4 + 70)), font, 2, color,3)  # text,coordinate,font,size of text,color,thickness of font
+
+    cv2.imshow('', env)
+    cv2.waitKey(500)
+
+
+def move(game_settings):
+    env = environment(game_settings)
+    cv2.putText(env, "Relax ...", (int(game_settings['WIDTH'] / 2 - 70), int(game_settings['HEIGHT'] // 4)), font, 1, (255, 255, 255),2)  # text,coordinate,font,size of text,color,thickness of font
+    cv2.imshow('', env)
+    cv2.waitKey(1000)
+
+
+def generate_random_actions(n:int=20):
+    # Action to be performed
+    actions = np.full(n, "right")
+    actions = np.append(actions, np.full(n, "left"))
+    np.random.shuffle(actions)
+    return actions
+
+
+def input_process(trial, W_matrix):
+    trial = np.expand_dims(trial, axis=2)
+    n_channels = trial.shape[0]
+    nsamples_win = trial.shape[1]
+    sample_rate = 125
+
+    filter_input = bandpass(trial, 8, 15, sample_rate, n_channels, nsamples_win)
+    output_mix = apply_mix(W_matrix, filter_input, n_channels, nsamples_win)
+    comp = np.array([0, -2])
+    output_mix = output_mix[comp, :, 0]
+    point = logvar(output_mix)
+
+    return point
+
+
+def predict_nn(input_nn, model, W_matrix):
+    input_nn = input_process(input_nn, W_matrix)
+    out = model.predict(input_nn.reshape(1, 2))
+    print(out)
+    output = np.rint(out)
+    return output
+
+
+def predict_lda(input_lda, c, d, W_matrix):
+    print("Predicting")
+    point = input_process(input_lda, W_matrix)
+    results = np.cross(point - d, c - d)
+    print(point,results)
+    # 1 left 0 right
+    if results < 0:
+        # above the line
+        output = 0
+    else:
+        output = 1
+
+    return output
+
+
+## ------------ MAIN ------------------------------
+def main(n_samples:int=20):
+    # Get game settings
+    WIDTH = 800
+    HEIGHT = 600
+    SQ_SIZE = 50
+    MOVE_SPEED = 1
+    game_settings = create_game_settings(WIDTH, HEIGHT, SQ_SIZE)
+
+    # Where data will be stored
+    date = datetime.today().strftime('%Y-%m-%d')
+    data_dir = "data/" + "raw/" + USER+ "/" + date
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    # Load model
+    if BOX_MOVE == "model":
+        MODEL_NAME = "models_07/NN/64.15-50epoch-1657286289-loss-0.64.model "
+        model = tf.keras.models.load_model(MODEL_NAME)
+
+        with open("/home/nurife/BCI/IM/BCI/models_09/W_matrix/1657359658.pickle", "rb") as file:
+            W_load = pickle.load(file)
+        W_matrix = W_load["W_matrix"]
+
+        with open("/home/nurife/BCI/IM/BCI/models_09/LDA/50.0_r_62.5_acc_1657359658.pickle", "rb") as file:
+            LDA_model = pickle.load(file)
+        W = LDA_model["W"]
+        b = LDA_model["b"]
+
+        x = np.array([-5, 1])
+        y = (b - W[0] * x) / W[1]
+        d = np.array([x[0], y[0]])
+        c = np.array([x[1], y[1]])
+
+    # Get datastream: resolve an EEG stream on the lab network
+    print("Looking for an EEG stream...")
+    streams = resolve_stream('type', 'EEG')
+    # create a new inlet to read from the stream
+    inlet = StreamInlet(streams[0])
+
+    # Get random actions to be performed
+    actions = generate_random_actions(n_samples)
+
+    # Start recording
+    total = 0
+    left = 0
+    right = 0
+    none = 0
+    correct = 0
+    channel_datas = []
+    start = time.time()
+    for j in range(len(actions)):
+        # Update Game
+        update(j, game_settings, actions)
+        channel_data = {}
+        time_stamps = [0]
+        first_timestamp = 0
+        print("Start recording Data")
+
+        # Record for 5 seconds
+        while time_stamps[-1] < 5:
+            # Get sample fomr EEG stream
+            sample, timestamp = inlet.pull_sample()
+            timestamp = time.time()
+            if timestamp > first_timestamp + time_stamps[-1]:
+                for i in range(16):
+                    if i not in channel_data:
+                        channel_data[i] = [sample[i]]
+                    else:
+                        channel_data[i].append(sample[i])
+                if first_timestamp == 0:
+                    first_timestamp = timestamp
+                time_stamps.append(timestamp - first_timestamp)
+                print(timestamp-first_timestamp)
+
+        print("End recording Data")
+        channels = DataFrame()
+        channels["s"] = time_stamps[1:]
+        channels["mrk"] = actions[j]
+        for i in range(len(channel_data)):
+            channels[i] = channel_data[i]
+
+        if BOX_MOVE == "random":
+            box = actions[j]
+            box_shift = int((0.8*game_settings['HEIGHT'])//n_samples)
+            if box == "left":
+                game_settings['square_1']['y1'] -= box_shift
+                game_settings['square_1']['y2'] -= box_shift
+            elif box == "right":
+                game_settings['square_2']['y1'] -= box_shift
+                game_settings['square_2']['y2'] -= box_shift
+
+        elif BOX_MOVE == "model":
+            choice = predict_lda(np.asarray(channels)[:460, 2:].astype('float32').transpose(), c, d, W_matrix)
+
+            if choice == 1:
+                print(f"Action:{actions[j]} Moving:left")
+                game_settings['square_1']['y1'] -= 20
+                game_settings['square_1']['y2'] -= 20
+                left += 1
+            elif choice == 0:
+                print(f"Action:{actions[j]} Moving:right")
+                game_settings['square_2']['y1'] -= 20
+                game_settings['square_2']['y2'] -= 20
+                right += 1
+
+        move(game_settings)
+        time.sleep(10)
+        total += 1
+        curr_time = int(time.time())
+        save_path = os.path.join(f"{data_dir}/", f"{actions[j]}_{j}_{curr_time}.csv")
+        channels.to_csv(save_path)
+
+        channel_datas.append(channels)
+
+    # plt.plot(channel_datas[0][0])
+    # plt.show()
+
+    print(time.time() - start)
+    print(len(channel_datas))
+    print("Done.")
+
+    print(USER, correct / total)
+    print(f"left: {left / total}, right: {right / total}")
+
+    with open("accuracies.csv", "a") as f:
+        f.write(
+            f"{int(time.time())},{USER},{correct / total},{MODEL_NAME},{left / total},{right / total}\n")
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/data/signal_processing.py
@@ -0,0 +1,249 @@
+import numpy as np
+import scipy
+from matplotlib import mlab
+from scipy import linalg, integrate
+from scipy.signal import freqs
+
+
+def cov(trials, nsamples):
+    """ Calculate the covariance for each trial and return their average """
+    ntrials = trials.shape[2]
+    covariances = [trials[:, :, i].dot(trials[:, :, i].T) / nsamples for i in range(ntrials)]
+    return np.mean(covariances, axis=0)
+
+
+def whitening(sigma):
+    """ Calculate a whitening matrix for covariance matrix sigma. """
+    U, l, _ = linalg.svd(sigma)
+    return U.dot(np.diag(l ** -0.5))
+
+
+def psd(trials, nchannels, nsamples, sample_rate):
+    """
+    Calculates for each trial the Power Spectral Density (PSD).
+
+    Parameters
+    ----------
+    trials : 3d-array (channels x samples x trials)
+        The EEG signal
+
+    Returns
+    -------
+    trial_PSD : 3d-array (channels x PSD x trials)
+        the PSD for each trial.
+    freqs : list of floats
+        Yhe frequencies for which the PSD was computed (useful for plotting later)
+        :param nsamples:
+        :param sample_rate:
+        :param trials:
+        :param nchannels:
+    """
+
+    ntrials = trials.shape[2]
+    trials_PSD = np.zeros((nchannels, 148, ntrials))
+    # Iterate over trials and channels
+    for trial in range(ntrials):
+        for ch in range(nchannels):
+            # Calculate the PSD
+            (PSD, frequency) = mlab.psd(trials[ch, :, trial], NFFT=int(nsamples), Fs=sample_rate)
+            trials_PSD[ch, :, trial] = PSD.ravel()
+
+    return trials_PSD, frequency
+
+
+def bandpass(trials, lo, hi, sample_rate, nchannels, nsamples):
+    """
+    Designs and applies a bandpass filter to the signal.
+
+    Parameters
+    ----------
+    trials : 3d-array (channels x samples x trials)
+        The EEG signal
+    lo : float
+        Lower frequency bound (in Hz)
+    hi : float
+        Upper frequency bound (in Hz)
+    sample_rate : float
+        Sample rate of the signal (in Hz)
+
+    Returns
+    -------
+    trials_filt : 3d-array (channels x samples x trials)
+    """
+
+    # The iirfilter() function takes the filter order: higher numbers mean a sharper frequency cutoff,
+    # but the resulting signal might be shifted in time, lower numbers mean a soft frequency cutoff,
+    # but the resulting signal less distorted in time. It also takes the lower and upper frequency bounds
+    # to pass, divided by the nyquist frequency, which is the sample rate divided by 2:
+    a, b = scipy.signal.iirfilter(6, [lo / (sample_rate / 2.0), hi / (sample_rate / 2.0)])
+
+    # Applying the filter to each trial
+
+    ntrials = trials.shape[2]
+
+    trials_filt = np.zeros((nchannels, nsamples, ntrials))
+    for i in range(ntrials):
+        trials_filt[:, :, i] = scipy.signal.filtfilt(a, b, trials[:, :, i], axis=1)
+
+    return trials_filt
+
+
+def logvar(trials):
+    """
+    Calculate the log-var of each channel.
+
+    Parameters
+    ----------
+    trials : 3d-array (channels x samples x trials)
+        The EEG signal.
+
+    Returns
+    -------
+    jmnar - 2d-array (channels x trials)
+        For each channel the logvar of the signal
+    """
+    return np.log(np.var(trials, axis=1))
+
+
+def csp(trials_r, trials_l, nsamples):
+    """
+    Calculate the CSP transformation matrix W.
+    arguments:
+        trials_r - Array (channels x samples x trials) containing right-hand movement trials
+        trials_l - Array (channels x samples x trials) containing left-hand movement trials
+    returns:
+        Mixing matrix W
+    """
+
+    cov_r = cov(trials_r, nsamples)
+    cov_l = cov(trials_l, nsamples)
+
+    P = whitening(cov_r + cov_l)
+
+    B, _, _ = linalg.svd(P.T.dot(cov_l).dot(P))
+    W = P.dot(B)
+    # spatial filters (maximise one variance, minimise other)
+    return W
+
+
+def apply_mix(W, trials, nchannels, nsamples):
+    """ Apply a mixing matrix to each trial (basically multiply W with the EEG signal matrix)"""
+    ntrials = trials.shape[2]
+    trials_csp = np.zeros((nchannels, nsamples, ntrials))
+    for i in range(ntrials):
+        trials_csp[:, :, i] = W.T.dot(trials[:, :, i])
+    return trials_csp
+
+
+def integration(y_data=None, chan_lab=None, cl=None, trials_PSD=None, ch=None):
+    cl = list(trials_PSD.keys())[0]
+    y_data = np.mean(trials_PSD[cl][ch, :, :], axis=1)
+
+    integration = integrate.trapz(y_data, freqs)
+    print('Channel %s, Class %s >> %f' % (chan_lab, cl, integration))
+    return integration
+
+
+def train_lda(class1, class2):
+    '''
+    Trains the LDA algorithm.
+    arguments:
+        class1 - An array (observations x features) for class 1
+        class2 - An array (observations x features) for class 2
+    returns:
+        The projection matrix W
+        The offset b
+    '''
+    nclasses = 2
+
+    nclass1 = class1.shape[0]
+    nclass2 = class2.shape[0]
+
+    # Class priors: in this case, we have an equal number of training
+    # examples for each class, so both priors are 0.5
+    prior1 = nclass1 / float(nclass1 + nclass2)
+    prior2 = nclass2 / float(nclass1 + nclass1)
+
+    mean1 = np.mean(class1, axis=0)
+    mean2 = np.mean(class2, axis=0)
+
+    class1_centered = class1 - mean1
+    class2_centered = class2 - mean2
+
+    # Calculate the covariance between the features
+    cov1 = class1_centered.T.dot(class1_centered) / (nclass1 - nclasses)
+    cov2 = class2_centered.T.dot(class2_centered) / (nclass2 - nclasses)
+
+    W = (mean2 - mean1).dot(np.linalg.pinv(prior1 * cov1 + prior2 * cov2))
+    b = (prior1 * mean1 + prior2 * mean2).dot(W)
+
+    return W, b
+
+
+def best_components(trials_log):
+    y0 = np.mean(trials_log["left"], axis=1)
+    y1 = np.mean(trials_log["right"], axis=1)
+    diff = []
+    for i, y in enumerate(y0):
+        diff.append(y - y1[i])
+
+    # plt.figure(figsize=(12, 5))
+    # lft = np.zeros(len(diff))
+    # rght = np.zeros(len(diff))
+    #
+    # for i, value in enumerate(diff):
+    #     if value > 0:
+    #         rght[i] = value
+    #     else:
+    #         lft[i] = value
+    #
+    # x0 = np.arange(len(diff))
+
+    # plt.bar(x0, rght, width=0.5, color="r")
+    # plt.bar(x0, lft, width=0.5, color="b")
+    # plt.xlim(-0.5, len(diff) + 0.5)
+    # plt.title("Difference of LogVar after CSP")
+    # plt.xlabel("Components")
+    # plt.ylabel("LogVariance")
+    # plt.legend(["right", "left"])
+    # plt.gca().yaxis.grid(True)
+    # plt.show()
+
+    pc_right = np.where(diff == np.amax(diff))[0]
+    pc_left = np.where(diff == np.amin(diff))[0]
+    return int(pc_right), int(pc_left)
+
+
+def accuracies(trials_acc, W_value, b_value):
+    left_wrong = 0
+    left_correct = 0
+    right_wrong = 0
+    right_correct = 0
+
+    x = np.array([-5, 1])
+    y = (b_value - W_value[0] * x) / W_value[1]
+
+    a = np.array([x[0], y[0]])
+    c = np.array([x[1], y[1]])
+
+    lt = trials_acc["left"].transpose()
+    rt = trials_acc["right"].transpose()
+
+    for point in lt:
+        results = np.cross(point - a, c - a)
+        if results > 0:
+            left_correct += 1
+        else:
+            left_wrong += 1
+
+    for point in rt:
+        results = np.cross(point - a, c - a)
+        if results < 0:
+            right_correct += 1
+        else:
+            right_wrong += 1
+
+    #print(f"Left {left_correct / len(lt)} Right: {right_correct / len(rt)}")
+
+    total = float((left_correct / (len(lt)) + right_correct / (len(rt))) / 2)
+    return total
